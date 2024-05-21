40f278fd73a7abd827ebacc3db8df28968869296
dongyoon7212 <dongyoon7212@naver.com>
2024-05-21 17:52:41 +0900

    Init: 프로젝트 초기 셋팅

---
+++ src/main/java/com/travelertown/travelertown/TravelertownApplication.java
@@ -0,0 +1,13 @@
+package com.travelertown.travelertown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelertownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelertownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelertown/travelertown/TravelertownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelertown.travelertown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelertownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
