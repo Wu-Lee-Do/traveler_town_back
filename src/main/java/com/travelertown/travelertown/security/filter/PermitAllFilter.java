146d85e6baf13a7755419fa167f615ab3632eca4
dongyoon7212 <dongyoon7212@naver.com>
2024-07-18 15:16:20 +0900

    Feat: permitAll Filter 수정

--- src/main/java/com/travelertown/travelertown/security/filter/PermitAllFilter.java
+++ src/main/java/com/travelertown/travelertown/security/filter/PermitAllFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Component
 public class PermitAllFilter extends GenericFilter {
@@ -29,19 +30,30 @@ public class PermitAllFilter extends GenericFilter {
                 "/board/bookmark/remove",
                 "/comment",
                 "/like",
-                "/board/bookmark/all"
+                "/board/bookmark/all",
+                "/board/",
+                "/board/all"
         );
 
         String uri = request.getRequestURI();
         request.setAttribute("isPermitAll", false);
-        for(String antMatcher : antMatchers) {
-            if(uri.startsWith(antMatcher)) {
+        for (String antMatcher : antMatchers) {
+            if (matches(uri, antMatcher)) {
                 request.setAttribute("isPermitAll", true);
+                break;  // 일치하는 패턴이 있으면 더 이상 확인하지 않음
             }
         }
 
         filterChain.doFilter(request, response);
 
 
+    }
+
+    private boolean matches(String uri, String pattern) {
+        if (pattern.equals("/board/")) {
+            return Pattern.matches("^/board/\\d+$", uri);  // 숫자로 끝나는 /board/ 경로 매칭
+        } else {
+            return uri.equals(pattern);
+        }
     }
 }
